19eececca426d87881033ba8090b6120e9300819
Ajay-Kr <ajaykumar1312.p@gmail.com>
2023-06-15 16:16:52 +0530

    Fixture page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Fixtures from 'pages/Fixtures';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Fixtures />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Fixtures/index.jsx
@@ -0,0 +1,15 @@
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import FixturesTable from 'components/FixturesTable';
+
+const Fixtures = () => {
+  return (
+    <div>
+      <Header />
+      <FixturesTable />
+      <Footer />
+    </div>
+  )
+}
+
+export default Fixtures
